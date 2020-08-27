a612044bc943cf19b5c235730b2a2fa354922a8b
Geng <katsunari0908@gmail.com>
2020-08-27 13:55:34 +0700

    Fixed typo in bidDB.sql.

--- bidDB.sql
+++ bidDB.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `bidDB`;
 
-USE `bidDB`
+USE `bidDB`;
+
 DROP TABLE IF EXISIS `customers`;
 DROP TABLE IF EXISIS `login`;
 DROP TABLE IF EXISIS `bid`;
@@ -10,28 +11,25 @@ DROP TABLE IF EXISIS `orders`;
 DROP TABLE IF EXISIS `order_details`;
 DROP TABLE IF EXISIS `payments`;
 
-
-
 CREATE TABLE `login`(
 `customer_id` AUTO_INCREMENT INT NOT NULL ,
-`username`VACHAR(120) NOT NULL,
-`password`VACHAR(120) NOT NULL,
-`email`VACHAR(120) NOT NULL UNIQUE,
+`username`VARCHAR(120) NOT NULL,
+`password`VARCHAR(120) NOT NULL,
+`email`VARCHAR(120) NOT NULL UNIQUE,
 
 PRIMARY KEY(`customer_id`)
-)) ENGING=InnoDB DEFAULT CHARSET utf8mb4;
-
+) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
 CREATE TABLE `customers`(
 `customer_id`INT NOT NULL ,
-`first_name`VACHAR(120) NOT NULL,
-`last_name`VACHAR(120) NOT NULL,
-`address`VACHAR(120) NOT NULL,
+`first_name`VARCHAR(120) NOT NULL,
+`last_name`VARCHAR(120) NOT NULL,
+`address`VARCHAR(120) NOT NULL,
 `phone_number`INT(10) NOT NULL UNIQUE,
 
 PRIMARY KEY(`customer_id`),
 CONSTRAINT `login_ibfk_1` FOREIGN KEY(`customer_id`)REFERENCES `login`,`customer_id`
-) ENGING=InnoDB DEFAULT CHARSET utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
 CREATE TABLE `bid`(
 `customer_id`INT NOT NULL ,
@@ -42,33 +40,27 @@ CREATE TABLE `bid`(
 PRIMARY KEY(`product_id`),
 CONSTRAINT `products_ibfk_1` FOREIGN KEY(`product_id`)REFERENCES `products`,`product_id`,
 CONSTRAINT `customers_ibfk_2` FOREIGN KEY(`customer_id`)REFERENCES `customers`,`customer_id`
-
-)) ENGING=InnoDB DEFAULT CHARSET utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
 CREATE TABLE `products`(
 `customer_id`INT NOT NULL ,
 `product_id`INT  NOT NULL AUTO_INCREMENT ,
-`product_name`VACHAR(120) NOT NULL,
+`product_name`VARCHAR(120) NOT NULL,
 `countdown`TIMESTAMP,
 `stock`INT NOT NULL,
 
-
 PRIMARY KEY(`product_id`),
 CONSTRAINT `customers_ibfk_1` FOREIGN KEY(`customer_id`)REFERENCES `customers`,`customer_id`
-
-)) ENGING=InnoDB DEFAULT CHARSET utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
 CREATE TABLE `products_details`(
 `product_id`INT  NOT NULL  ,
 `price`INT NOT NULL,
-`description`VACHAR(120) NOT NULL,
-
-
+`description`VARCHAR(120) NOT NULL,
 
 PRIMARY KEY(`product_id`),
 CONSTRAINT `products_ibfk_1` FOREIGN KEY(`product_id`)REFERENCES `products`,`product_id`,
-
-)) ENGING=InnoDB DEFAULT CHARSET utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
 CREATE TABLE `orders`(
 `order_id`INT  NOT NULL AUTO_INCREMENT ,
@@ -76,9 +68,7 @@ CREATE TABLE `orders`(
 
 PRIMARY KEY(`order_id`),
 CONSTRAINT `customers_ibfk_1` FOREIGN KEY(`customer_id`)REFERENCES `customers`,`customer_id`
-
-)) ENGING=InnoDB DEFAULT CHARSET utf8mb4;
-
+) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
 CREATE TABLE `order_details`(
 `order_id`INT  NOT NULL  ,
@@ -88,23 +78,15 @@ CREATE TABLE `order_details`(
 PRIMARY KEY(`order_id`),
 CONSTRAINT `orders_ibfk_1` FOREIGN KEY(`order_id`)REFERENCES `orders`,`order_id`,
 CONSTRAINT `products_ibfk_2` FOREIGN KEY(`product_id`)REFERENCES `products`,`product_id`
-
-
-
-
-)) ENGING=InnoDB DEFAULT CHARSET utf8mb4;
-
-
+) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
 CREATE TABLE `payments`(
 `order_id`INT  NOT NULL  ,
-`method`VACHAR(255) NOT NULL ,
-`status`VACHAR(255) NOT NULL ,
+`method`VARCHAR(255) NOT NULL ,
+`status`VARCHAR(255) NOT NULL ,
 `total`INT NOT NULL ,
 
 PRIMARY KEY(`order_id`),
 CONSTRAINT `orders_ibfk_1` FOREIGN KEY(`order_id`)REFERENCES `orders`,`order_id`
-
-)) ENGING=InnoDB DEFAULT CHARSET utf8mb4;
-
+) ENGINE=InnoDB DEFAULT CHARSET utf8mb4;
 
