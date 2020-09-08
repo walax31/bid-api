07e76ea20c5362de90b7110a16388937fb6f7931
Katsunari Miyajima <katsunari0908@gmail.com>
2020-09-09 00:09:52 +0700

    Replaced loginDetail with users and changed user primary key to user_id.

--- database/migrations/1599538572791_customer_schema.js
+++ database/migrations/1599538572791_customer_schema.js
@@ -8,15 +8,16 @@ class CustomerSchema extends Schema {
     this.create("customers", (table) => {
       table.increments("customer_id");
       table.timestamps();
+      table.integer("user_id").unsigned().notNullable();
       table.string("customer_first_name", 50).notNullable();
       table.string("customer_last_name", 50).notNullable();
       table.string("customer_address", 50).notNullable();
-      table.string("customer_phone", 10).notNullable();
+      table.string("customer_phone", 20).notNullable().unique();
       table.string("customer_path_to_credential").notNullable().unique();
 
       table
-        .foreign("customer_id")
-        .references("login_details.customer_id")
+        .foreign("user_id")
+        .references("users.user_id")
         .onDelete("CASCADE")
         .onUpdate("CASCADE");
     });
