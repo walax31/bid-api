8cddada728273789a34249ba14cc77a24a0268fb
walax31 <walax31009@gmail.com>
2020-09-09 16:36:29 +0700

    edit product controller

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -1,46 +1,44 @@
 "use strict";
 
-const Database = use(`Database`);
-
+const productValidator = require("../../../service/productValidator");
 const Product = use("App/Models/Product");
 const makeProductUtil = require("../../../util/ProductUtil.func");
-
-function numberTypeParamValidator(number) {
-  if (Number.isNaN(parseInt(number))) {
-    return {
-      error: `param:${number} is not supported,please use number type param intnstead`,
-    };
-  }
-  return {};
-}
+const numberTypeParamValidator = require("../../../util/numberTypeParamValidator.func");
 class ProductController {
   async index({ request }) {
-    const { references = undefined } = request.qs;
-    const product = await makeProductUtil(Product).getAll(references);
+    const { references } = request.qs;
+    const products = await makeProductUtil(Product).getAll(references);
 
-    return { status: 200, error: undefined, data: product };
+    return { status: 200, error: undefined, data: products };
   }
 
   async show({ request }) {
-    const { id } = request.params;
-    const { references } = request.qs;
+    const { params, qs } = request;
+
+    const { id } = params;
+
+    const { references } = qs;
 
     const validateValue = numberTypeParamValidator(id);
+
     if (validateValue.error)
       return { status: 500, error: validateValue.error, date: undefined };
 
-    const product = await makeProductUtil(Product).getAll(references);
+    const product = await makeProductUtil(Product).getById(references,id);
     return { status: 200, error: undefined, data: product || {} };
   }
   async store({ request }) {
-    const { customer_id, product_name, end_date, stock } = request.body;
+    const { body, qs } = request;
+    const { customer_id, product_name, end_date, stock } = body;
 
-    const rules = {
-      customer_id: "required",
-      product_name: "required",
-      end_date: "required",
-      stock: "required",
-    };
+    const { references } = qs;
+
+    const validation = await productValidator(request.body);
+
+    if (validation.error) {
+      return { status: 422, error: validation.error, data: undefined };
+    }
+
     const product = await makeProductUtil(Product).create(
       {
         customer_id,
@@ -48,44 +46,40 @@ class ProductController {
         end_date,
         stock,
       },
-      rules
+      references
     );
     return {
       status: 200,
       error: undefined,
-      data: customer_id,
-      product_name,
-      end_date,
-      stock,
+      data: product,
     };
   }
   async update({ request }) {
-    const { body, params } = request;
-    const { id } = params;
-    const { customer_id } = body;
-    const { product_name } = body;
-    const { end_date } = body;
-    const { stock } = body;
+    const { body, params, qs } = request;
 
-    const productID = await Database.table("products")
-      .where({ product_id: id })
-      .update({
-        customer_id,
-        product_name,
-        end_date,
-        stock,
-      });
-    const product = await Database.table("products")
-      .where({ product_id: productID })
-      .first();
+    const { id } = params;
+
+    const { references } = qs;
+
+    const { customer_id, product_name, end_date, stock } = body;
+
+    const product = await makeProductUtil(Product).updateById(
+      id,
+      { customer_id, product_name, end_date, stock },
+      references
+    );
 
     return { status: 200, error: undefined, data: product };
   }
   async destroy({ request }) {
     const { id } = request.params;
-    await Database.table("products").where({ product_id: id }).delete();
+    const product = await makeProductUtil(Product).deleteById(id);
 
-    return { status: 200, error: undefined, data: { massage: "success" } };
+    return {
+      status: 200,
+      error: undefined,
+      data: { massage: `${product} is successfully removed.` },
+    };
   }
 }
 
