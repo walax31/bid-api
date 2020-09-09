a3e95b21e11760f7af109f3fdc4584cbdc0e20b3
walax31 <walax31009@gmail.com>
2020-09-09 17:25:08 +0700

    edit test user

--- test/functional/001-user-controller.spec.js
+++ test/functional/001-user-controller.spec.js
@@ -1,7 +1,60 @@
 'use strict'
 
-const { test } = use('Test/Suite')('User Controller')
+const { test,trait } = use('Test/Suite')('User Controller')
+const UserModel =use("App/Models/User")
 
-test('make sure 2 + 2 is 4', async ({ assert }) => {
-  assert.equal(2 + 2, 4)
-})
+trait("Test/ApiClient");
+
+test("should return structured response with empty data array via get method.", async ({
+  client,
+}) => {
+  const response = await client.get("/api/v1/users").end();
+
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: [],
+  });
+});
+
+test("should return structured response with empty data via get method.", async ({
+  client,
+}) => {
+  const response = await client.get("api/v1/users/1").end();
+
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: {},
+  });
+});
+
+test("should return structured data with no references via post method.", async ({
+  client,
+}) => {
+  const UserData = {
+    username:"wala",
+    email:"wala@gmail.com",
+    password:"1234"
+  };
+
+  const response = await client.post("api/v1/users").send(UserData).end();
+
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: userData,
+  });
+});
+
+test("should return structured data with no references via put method.", async ({
+  client,
+}) => {
+  const userData = {
+    email:"wala@email.com"
+  };
+
+  const response = await client.put("api/v1/users/1").send(userData).end();
+
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: userData,
+  });
+});
