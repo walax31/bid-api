a34119057d77a2d237f4bca87af9b0183576cf24
Katsunari Miyajima <katsunari0908@gmail.com>
2020-09-09 22:03:59 +0700

    Fixed missing initializer in destructuring error in order and payment validator.

--- service/orderValidator.js
+++ service/orderValidator.js
@@ -4,10 +4,12 @@ module.exports = async function (data) {
   if (typeof data !== "object") throw new Error("Param donse is not an object");
 
   const { customer_id } = data;
-  const rules = {
-    customer_id: "require",
+
+  const rule = {
+    customer_id: "required",
   };
 
-  const validation = await Validator.validateAll({ customer_id }, rules);
+  const validation = await Validator.validateAll({ customer_id }, rule);
+
   return { error: validation.messages() };
 };
