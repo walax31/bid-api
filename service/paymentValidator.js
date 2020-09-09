059110e0a1ebca080580ae9da4014f933a373dd0
walax31 <walax31009@gmail.com>
2020-09-09 14:33:43 +0700

    add paymentValidation

---
+++ service/paymentValidator.js
@@ -0,0 +1,15 @@
+const Validator =use(Validator)
+module.exports =async function(data){
+    if(typeof data !=="object") throw new Error("Param dones is not a object.")
+   
+    const {method,status,total}
+    const rule={
+        method:"require",
+        status:"require",
+        total:"require"
+    }
+    const validation= await Validator.validateAll({
+        method,status,total
+    },rule)
+    return {error: validation.messages()}
+}
