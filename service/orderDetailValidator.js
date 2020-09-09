3d66b1ccd456de1894f97d39e9a0fdfff41f1f71
Katsunari Miyajima <katsunari0908@gmail.com>
2020-09-10 00:43:43 +0700

    Fixed miising initializer before destructuring error in orderDetailService.

--- service/orderDetailValidator.js
+++ service/orderDetailValidator.js
@@ -1,15 +1,25 @@
-const Validator =use(Validator)
-module.exports =async function(data){
-    if(typeof data !=="object") throw new Error("Param dones is not a object.")
-   
-    const {product_id,order_quantity,order_id}
-    const rule={
-        product_id:"require",
-        order_quantity:"require",
-        order_id:"require"
-    }
-    const validation= await Validator.validateAll({
-        product_id,order_quantity,order_id
-    },rule)
-    return {error: validation.messages()}
-}
+const Validator = use("Validator");
+
+module.exports = async function (data) {
+  if (typeof data !== "object") throw new Error("Param dones is not a object.");
+
+  const { product_id, order_quantity, order_id } = data;
+
+  const rules = {
+    product_id: "require",
+    order_quantity: "require",
+    order_id: "require",
+  };
+
+  const validation = await Validator.validateAll(
+    {
+      product_id,
+      order_quantity,
+      order_id,
+    },
+    rules
+  );
+
+  return { error: validation.messages() };
+};
+
