24a8dc6aafcf68070e38074a01a5481721a0feb4
walax31 <walax31009@gmail.com>
2020-09-10 20:16:07 +0700

    edit orderDetail validator

--- service/orderDetailValidator.js
+++ service/orderDetailValidator.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const Validator =use(Validator)
-module.exports =async function(data){
-    if(typeof data !=="object") throw new Error("Param dones is not a object.")
-   
-    const {product_id,order_quantity,order_id}=data
-    const rule={
-        product_id:"require",
-        order_quantity:"require",
-        order_id:"require"
-    }
-    const validation= await Validator.validateAll({
-        product_id,order_quantity,order_id
-    },rule)
-    return {error: validation.messages()}
-}
-=======
 const Validator = use("Validator");
 
 module.exports = async function (data) {
@@ -40,4 +23,3 @@ module.exports = async function (data) {
   return { error: validation.messages() };
 };
 
->>>>>>> 98147c566d0545d7e7b6879c8024e43fd03cf3fe
