8e9b3f4d7d9aaec0eea16f417bb053b472dfe290
walax31 <walax31009@gmail.com>
2020-09-11 22:25:07 +0700

    edit product-controller functional testing case independant from each other.

--- test/functional/007-product-controller.spec.js
+++ test/functional/007-product-controller.spec.js
@@ -1,14 +1,21 @@
-'use strict'
+"use strict";
 
-const { test,trait } = use('Test/Suite')('Product Controller')
-const UserModel =use("App/Models/User")
+const { test, trait } = use("Test/Suite")("Product Controller");
 
+const UserModel = use("App/Models/User");
+const CustomerModel = use("App/Models/Customer");
+const ProductModel = use("App/Models/Product");
+const makeUserUtil = require("../../util/testerUtil/autogenUserInstance.func");
+const makeCustomerUtil = require("../../util/testerUtil/autogenCustomerInstance.func");
+const makeProductUtil = require("../../util/testerUtil/autogenProductInstance.func");
 trait("Test/ApiClient");
+
+const urlEndPoint = "/api/v1/products";
 
 test("should return structured response with empty data array via get method.", async ({
   client,
 }) => {
-  const response = await client.get("api/v1/products").end();
+  const response = await client.get(urlEndPoint).end();
 
   response.assertStatus(200);
   response.assertJSONSubset({
@@ -19,7 +26,7 @@ test("should return structured response with empty data array via get method.", 
 test("should return structured response with empty data via get method.", async ({
   client,
 }) => {
-  const response = await client.get("api/v1/products/1").end();
+  const response = await client.get(`${urlEndPoint}/1`).end();
 
   response.assertStatus(200);
   response.assertJSONSubset({
@@ -27,34 +34,224 @@ test("should return structured response with empty data via get method.", async 
   });
 });
 
-test("should return structured data with no references via post method.", async ({
+test("should return error message and status code of 422 when field data is missing.", async ({
   client,
 }) => {
-  const ProductData = {
-    customer_id:2,
-    product_name:"waxcsfs",
+  const { user_id } = await makeUserUtil(UserModel);
 
+  const { customer_id } = await makeCustomerUtil(CustomerModel, user_id);
+
+  const product = {
+    stock: 1,
   };
 
-  const response = await client.post("api/v1/products").send(ProductData).end();
+  const response = await client.post(urlEndPoint).send(product).end();
 
   response.assertStatus(200);
   response.assertJSONSubset({
-    data: productDeta,
+    status: 422,
   });
+
+  await UserModel.find(user_id).then((response) => response.delete());
+});
+
+test("should return structured response with no references in an array via get method.", async ({
+  client,
+}) => {
+  const { user_id } = await makeUserUtil(UserModel);
+
+  const { customer_id } = await makeCustomerUtil(CustomerModel, user_id);
+
+  const product = await makeProductUtil(ProductModel, customer_id);
+
+  const response = await client.get(urlEndPoint).end();
+
+  console.log(response);
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: [product],
+  });
+
+  await UserModel.find(user_id).then((response) => response.delete());
+});
+
+test("should return structured response with references in an array via get method.", async ({
+  client,
+}) => {
+  const { user_id } = await makeUserUtil(UserModel);
+
+  const { customer_id } = await makeCustomerUtil(CustomerModel, user_id);
+
+  const product = await makeProductUtil(ProductModel, customer_id);
+
+  const response = await client
+    .get(urlEndPoint)
+    .query({ references: "customer" })
+    .end();
+
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: [
+      {
+        customer: { customer_id: customer_id },
+      },
+    ],
+  });
+
+  await UserModel.find(user_id).then((response) => response.delete());
+});
+
+test("should return structured response with no references via get method.", async ({
+  client,
+}) => {
+  const { user_id } = await makeUserUtil(UserModel);
+
+  const { customer_id } = await makeCustomerUtil(CustomerModel, user_id);
+
+  const product = await makeProductUtil(ProductModel, customer_id);
+
+  const response = await client
+    .get(`${urlEndPoint}/${product.product_id}`)
+    .end();
+
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: product,
+  });
+
+  await UserModel.find(user_id).then((response) => response.delete());
+});
+
+test("should return structured response with references via get method.", async ({
+  client,
+}) => {
+  const { user_id } = await makeUserUtil(UserModel);
+
+  const { customer_id } = await makeCustomerUtil(CustomerModel, user_id);
+
+  const product = await makeProductUtil(ProductModel, customer_id);
+
+  const response = await client
+    .get(`${urlEndPoint}/${product.product_id}`)
+    .query({ references: "customer" })
+    .end();
+
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: {
+      customer: { customer_id: customer_id },
+    },
+  });
+
+  await UserModel.find(user_id).then((response) => response.delete());
+});
+
+test("should return structured data with no references via post method.", async ({
+  client,
+}) => {
+  const { user_id } = await makeUserUtil(UserModel);
+
+  const { customer_id } = await makeCustomerUtil(CustomerModel, user_id);
+
+  const product = {
+    customer_id: customer_id,
+    product_name: "sdsdas",
+    stock: 3,
+  };
+
+  const response = await client.post(urlEndPoint).send(product).end();
+  console.log(response);
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: product,
+  });
+
+  await UserModel.find(user_id).then((response) => response.delete());
+});
+
+test("should return structured data with references via post method.", async ({
+  client,
+}) => {
+  const { user_id } = await makeUserUtil(UserModel);
+
+  const { customer_id } = await makeCustomerUtil(CustomerModel, user_id);
+  const product = {
+    customer_id: customer_id,
+    product_name: "sdsdas",
+    stock: 3,
+  };
+
+  const response = await client
+    .post(urlEndPoint)
+    .send(product)
+    .query({ references: "customer" })
+    .end();
+
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: product,
+  });
+
+  await UserModel.find(user_id).then((response) => response.delete());
 });
 
 test("should return structured data with no references via put method.", async ({
   client,
 }) => {
-  const productDeta = {
-    product_name:"ujk"
-  };
+  const { user_id } = await makeUserUtil(UserModel);
 
-  const response = await client.put("api/v1/products/1").send(productDeta).end();
+  const { customer_id } = await makeCustomerUtil(CustomerModel, user_id);
+
+  const product = await makeProductUtil(ProductModel, customer_id);
+
+  const response = await client
+    .put(`${urlEndPoint}/${product.product_id}`)
+    .send({ stock: 3 })
+    .end();
 
   response.assertStatus(200);
   response.assertJSONSubset({
-    data: productDetas,
+    data: { stock: 3 },
   });
+
+  await UserModel.find(user_id).then((response) => response.delete());
+});
+
+test("should return structured data with references via put method.", async ({
+  client,
+}) => {
+  const { user_id } = await makeUserUtil(UserModel);
+
+  const { customer_id } = await makeCustomerUtil(CustomerModel, user_id);
+
+  const product = await makeProductUtil(ProductModel, customer_id);
+
+  const response = await client
+    .put(`${urlEndPoint}/${product.product_id}`)
+    .send({ stock: 3  })
+    .query({ references: "customer" })
+    .end();
+
+  response.assertStatus(200);
+  response.assertJSONSubset({
+    data: { stock: 3  },
+  });
+
+  await UserModel.find(user_id).then((response) => response.delete());
+});
+
+test("should return data index via delete method.", async ({ client }) => {
+  const { user_id } = await makeUserUtil(UserModel);
+
+  const { customer_id } = await makeCustomerUtil(CustomerModel, user_id);
+
+  const product = await makeProductUtil(ProductModel, customer_id);
+
+  const response = await client
+    .delete(`${urlEndPoint}/${product.product_id}`)
+    .end();
+
+  response.assertStatus(200);
+
+  await UserModel.find(user_id).then((response) => response.delete());
 });
