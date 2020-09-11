e30439fe5aea033fb7742993652921ab284b32ee
walax31 <walax31009@gmail.com>
2020-09-11 22:28:43 +0700

    edit autogenProduct

--- util/testerUtil/autogenProductInstance.func.js
+++ util/testerUtil/autogenProductInstance.func.js
@@ -2,7 +2,7 @@ module.exports = function (ProductModel, customer_id) {
   return ProductModel.create({
     customer_id,
     product_name: "product_name",
-    end_date: new Date(),
+    // end_date: new Date(),
     stock: 10,
   }).then((response) => response["$attributes"]);
 };
