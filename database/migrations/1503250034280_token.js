19cf8f892e3d8cd5f034f6116ac858bc5226bc9c
Katsunari Miyajima <katsunari0908@gmail.com>
2020-09-12 17:09:47 +0700

    Changed foreign key syntax in token schema.

--- database/migrations/1503250034280_token.js
+++ database/migrations/1503250034280_token.js
@@ -7,15 +7,17 @@ class TokensSchema extends Schema {
   up() {
     this.create("tokens", (table) => {
       table.increments("token_id");
-      table
-        .integer("user_id")
-        .unsigned()
-        .references("user_id")
-        .inTable("users");
+      table.integer("user_id").unsigned();
       table.string("token", 255).notNullable().unique().index();
       table.string("type", 80).notNullable();
       table.boolean("is_revoked").defaultTo(false);
       table.timestamps();
+
+      table
+        .foreign("user_id")
+        .references("users.user_id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
     });
   }
 
