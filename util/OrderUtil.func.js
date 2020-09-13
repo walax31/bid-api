083e189c1d945b9852a85ec06a0e7906d135e9aa
Katsunari Miyajima <katsunari0908@gmail.com>
2020-09-13 15:08:47 +0700

    Added pagination and moved validation case to other util function in order util function.

--- util/OrderUtil.func.js
+++ util/OrderUtil.func.js
@@ -11,42 +11,6 @@ module.exports = function (OrderModel) {
     return _Order;
   };
 
-  const _findProductOnAuthUser = (UserModel, user_id, product_id) => {
-    return UserModel.query()
-      .with("customer.products", (builder) => {
-        builder.where({ customer_id: user_id, product_id });
-      })
-      .where({ user_id })
-      .fetch()
-      .then((response) =>
-        response.first().getRelated("customer").getRelated("products").first()
-      );
-  };
-
-  const _findExistingOrder = (UserModel, customer_id, product_id) => {
-    return UserModel.query()
-      .with("customer.orders", (builder) => {
-        builder.where({ customer_id, product_id });
-      })
-      .where({ user_id: customer_id })
-      .fetch()
-      .then((response) =>
-        response.first().getRelated("customer").getRelated("orders").first()
-      );
-  };
-
-  const _findExistingBidForThisProduct = (UserModel, user_id, product_id) => {
-    return UserModel.query()
-      .with("customer.bids.product", (builder) => {
-        builder.where({ product_id });
-      })
-      .where({ user_id })
-      .fetch()
-      .then((response) =>
-        response.first().getRelated("customer").getRelated("bids").first()
-      );
-  };
-
   return {
     getAll: (references, customer_id) => {
       if (customer_id)
@@ -60,47 +24,13 @@ module.exports = function (OrderModel) {
         .fetch()
         .then((response) => response.first());
     },
-    create: async (
-      attributes,
-      UserModel,
-      auth_id,
-      customer_id,
-      product_id,
-      references
-    ) => {
-      if (!(await _findProductOnAuthUser(UserModel, auth_id, product_id)))
-        return {
-          status: 403,
-          error_msg: "cannot initiate order for product you don't own.",
-        };
-
-      if (
-        !(await _findExistingBidForThisProduct(
-          UserModel,
-          customer_id,
-          product_id
-        ))
-      )
-        return {
-          status: 404,
-          error_msg: "this user never put a bid on your product.",
-        };
-
-      if (await _findExistingOrder(UserModel, customer_id, product_id))
-        return {
-          status: 500,
-          error_msg: "order on this specific user has already existed.",
-        };
-
+    create: async (attributes, references) => {
       const { order_id } = await OrderModel.create(attributes);
 
-      return {
-        status: 200,
-        data: _withReferences(references)
-          .where({ order_id })
-          .fetch()
-          .then((response) => response.first()),
-      };
+      return _withReferences(references)
+        .where({ order_id })
+        .fetch()
+        .then((response) => response.first());
     },
     updateById: async (order_id, attributes, references) => {
       let order = await OrderModel.find(order_id);
