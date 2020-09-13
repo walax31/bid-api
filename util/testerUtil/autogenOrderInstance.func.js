58e09e9657bc6cccdf2ceb9a5d163ddaa9f2b897
Katsunari Miyajima <katsunari0908@gmail.com>
2020-09-13 15:11:16 +0700

    Cleaned up code in autogen order test/util function.

--- util/testerUtil/autogenOrderInstance.func.js
+++ util/testerUtil/autogenOrderInstance.func.js
@@ -1,11 +1,7 @@
-module.exports = function (OrderModel, customer_id,product_id) {
+module.exports = function (OrderModel, customer_id, product_id) {
   return OrderModel.create({
-     customer_id,
-     product_id,
-     order_quantity:10,
-    
-    
-    }).then(
-    (response) => response["$attributes"]
-  );
+    customer_id,
+    product_id,
+    order_quantity: 10,
+  }).then((response) => response["$attributes"]);
 };
