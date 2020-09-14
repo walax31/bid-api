6ff2c229fcda93b9f4b10152811f967ce158f11a
Katsunari Miyajima <katsunari0908@gmail.com>
2020-09-14 22:58:58 +0700

    Added order_id as requirement.

--- service/paymentValidator.js
+++ service/paymentValidator.js
@@ -3,17 +3,19 @@ const Validator = use("Validator");
 module.exports = async function (data) {
   if (typeof data !== "object") throw new Error("Param dones is not a object.");
 
-  const { method, status, total } = data;
+  const { method, status, total, order_id } = data;
 
   const rules = {
     method: "required",
     total: "required",
+    order_id: "required",
   };
 
   const validation = await Validator.validateAll(
     {
       method,
       total,
+      order_id,
     },
     rules
   );
