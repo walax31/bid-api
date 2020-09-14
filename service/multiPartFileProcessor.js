e706ac6c11c1cb2a65dc839e459d4690b2aaab2f
Katsunari Miyajima <katsunari0908@gmail.com>
2020-09-14 13:00:53 +0700

    Added urlProcessor service.

--- service/multiPartFileProcessor.js
+++ service/multiPartFileProcessor.js
@@ -1,16 +1,32 @@
-const Helpers = use("Helpers");
+module.exports = async function (Drive, rows) {
+  const new_rows = await rows.map(async (row) => {
+    const {
+      user_id,
+      first_name,
+      last_name,
+      address,
+      phone,
+      path_to_credential,
+      customer_id,
+    } = row;
 
-module.exports = async function (credentialImage, username) {
-  const fileName = `${new Date().getTime()}-${username}`;
+    if (path_to_credential)
+      return {
+        user_id,
+        first_name,
+        last_name,
+        address,
+        phone,
+        path_to_credential: path_to_credential
+          ? await Drive.disk("s3").getSignedUrl(path_to_credential)
+          : undefined,
+        customer_id,
+        user: row.getRelated("user"),
+        tokens: row.getRelated("tokens"),
+      };
 
-  await credentialImage.move(Helpers.tmpPath("uploads"), {
-    name: `${fileName}.jpg`,
-    overwrite: true,
+    return row;
   });
 
-  if (!credentialImage.moved()) {
-    return { error: credentialImage.error() };
-  }
-
-  return { data: fileName };
+  return new_rows;
 };
